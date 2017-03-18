77257b819d752ab2d63c78467d3b410209073892
contactsamie <contactsamie@gmail.com>
2017-03-18 19:51:33 -0400

    update

---
+++ EasyExecuteSample/EasyExecuteSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyExecuteSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new EasyExecuteLib.EasyExecute();
+            service.ExecuteAsync("1", () =>
+            {
+                Console.WriteLine("Hello world");
+                return Task.FromResult(new object());
+            }).Wait();
+        }
+    }
+}
